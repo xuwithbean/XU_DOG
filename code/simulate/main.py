12e488e8c1d6fee30b3c6a2eb35bd0324723a8d4
xuwithbean <820558752@qq.com>
2023-11-19 18:19:38 +0800

    update console in main.py

--- code/simulate/main.py
+++ code/simulate/main.py
@@ -1,87 +1,139 @@
 import os
+import time 
 import numpy as np
-from matplotlib import pyplot as plt
-import realtime
+import threading as trd
+import matplotlib as plt
+class timesim:
+    def print_time(threadName, delay, counter):
+        while counter:
+            time.sleep(delay)
+            print ("%s: %s" % (threadName, time.ctime(time.time())))
+            counter -= 1
+class simthread:
+    threads=[]
+    class timethread(trd.Thread):
+        def __init__(self, threadID, name, delay):
+            trd.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.delay = delay
+        def run(self):
+            print ("start thread" + self.name)
+            trd.Lock().acquire()
+            timesim.print_time(self.name, self.delay, 2)
+            trd.Lock().release()
+            timethread1=simthread.timethread(1,"timethread1",1)
+            timethread1.start()
+            simthread.threads.append(timethread1)
+        def stop(self):
+            self.join()
+            print("quit timthread")
 
-class quadruped_robot:
-    width = 15*2
-    length = 41*2
-    shoulderlen = 0
-    thighlen = 0
-    shanklen = 0
-    id = ["ftlf","frrt","bhlf","bhrt"]
-    leg_pos = [(width,length),(-width,length),(width,-length),(-width,-length)]
-
-class valuecount:
-    id = ["ftlf","frrt","bhlf","bhrt"]
-    shoulder = [0,0,0,0]
-    thigh = [0,0,0,0]
-    shank = [0,0,0,0]
-
-class log:
-    time = ''
-    value=['','','','','']
-
-def drawleg(id,shoulder,thigh,shank):
-    if id == quadruped_robot.id[0]:
-        print("still coding")
-    elif id == quadruped_robot.id[1]:
-        print("still coding")
-    elif id == quadruped_robot.id[2]:
-        print("still coding")
-    elif id == quadruped_robot.id[3]:
-        print("still coding")
-
-
-def readlog(filex):
-    with open(filex,'r') as f:
-        line = f.readline()
-        print("the record's date is\n")
-        print(line)
-        while 1:
-            line = f.readline()
-            if len(line) == 0:
-                print("log read finished")
-                break
-            ang = line.split(',')
-            if len(ang) != 5:
-                break
-            cnt=0
-            log.value=[0,0,0,0,'']
-            while cnt<=4:               
-                log.value[cnt]=float(ang[cnt])
-                cnt=cnt+1
-            if log.value[5] != '#':
-                break
-
-def playanim(logfile):
-    fig = plt.figure()
-    coordinate = fig.add_subplot(111,projection="3d")
-    coordinate.scatter([0],[0],[0],"r")
-    coordinate.set_xlim(-50,50)
-    coordinate.set_ylim(-50,50)
-    coordinate.set_zlim(-50,50)
-    readlog(logfile)
-    i = 0
-    while i <= 3:
-        drawleg(valuecount.id[i],valuecount.shoulder[i],valuecount.thigh[i],valuecount.shank[i])
-        plt.pause(1)
-        i = i+1
-if __name__ =='__main__':
-    print(" \r-------------------------------------\n\
-            \rplease input mode\n\
-            \rp is play the past log's anime\n\
-            \rr is play the realtime dog's anime\n\
-            \rq is quit the function\n\
-            \r--------------------------------------")
-    cmd = input("there to input mode(p/r/q)")
-    if (cmd == 'p'):
-        print("past log\n")
-        playanim('runcode/simluate/log/pastlog.txt')
-    elif (cmd == 'r'):
-        print("reatime mode\n")
-        realtime.realtimerecode('runcode/simluate/log/realtime.txt')
-    elif (cmd == 'q'):
-        print("function is endding...\n")
+    class consolethread(trd.Thread):
+        def __init__(self, threadID, name, delay):
+            trd.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.delay = delay
+        def run(self):
+            print ("start thread" + self.name)
+            trd.Lock().acquire()
+            console.all()
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+            trd.Lock().release()
+            consolethread1=simthread.consolethread(1,"consolethread1",1)
+            consolethread1.start()
+            simthread.threads.append(consolethread1)
+        def stop(self):
+            self.join()
+            print("quit consolethread")
+    def stop():
+        for t in simthread.threads:
+            t.join()
+        print ("quit all threads...")
+        
+    
+class console:
+    cmd=''
+    statuspast=0
+    statusreal=0
+    def all():
+        print(" \r-----------------console-----------------\n\
+                \rinput H/h to show this shell\n\
+                \rinput Q/q to quit the function\n\
+                \rinput P/p to play the past time's simulation\n\
+                \rinput R/r to play the real time's simulation\n\
+                \r----------------- status -----------------\n\
+                \r0 refers to no\n\
+                \r1 refers to yes\n\
+                \rnow:pastsimulation:0,realsimulation:0\n\
+                \r-----------------------------------------")
+    def notmatch():
+        print(" \rthe command you scaned in is incorrect\n\
+                \rplease use 'H/h' to watch the help document\n")
+    def getstatus(p,r):
+        print(" \r----------------- status -----------------\n\
+                \r0 refers to no\n\
+                \r1 refers to yes\n\
+                \rnow:pastsimulation:%d,realsimulation:%d\n\
+                \r-----------------------------------------\n" %(p,r))
+    def help():
+        print(" \r-----------------console-----------------\n\
+                \rinput H/h to show this shell\n\
+                \rinput Q/q to quit the function\n\
+                \rinput S/s to stop the code\n\
+                \rinput P/p to play the past time's simulation\n\
+                \rinput R/r to play the real time's simulation\n\
+                \r-----------------------------------------\n")
+    def stop(p,r):
+        print(" \r----------------- status -----------------\n\
+                \r0 refers to no\n\
+                \r1 refers to yes\n\
+                \rnow:pastsimulation:%d,realsimulation:%d\n\
+                \r-----------------------------------------\n" %(p,r))
+    def quit():
+        print("code is stopping now...")
         os._exit(0)
-    print("ok")
+    def keyboard():
+        console.cmd=input("please input the cmd now\n")
+    def cmdsfind(c):
+        if(c=='H' or c=='h'):
+            console.help()
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+        elif(c=='Q' or c=='q'):
+            console.quit()
+        elif(c=='P' or c=='p'):
+            console.statuspast=1
+            console.statusreal=0
+            console.getstatus(console.statuspast,console.statusreal)
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+        elif(c=='R' or c=='r'):
+            console.statuspast=0
+            console.statusreal=1
+            console.getstatus(console.statuspast,console.statusreal)
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+        elif(c=='S' or c=='s'):
+            print("code is stopping now...")
+            console.statuspast=0
+            console.statusreal=0
+            console.stop(console.statuspast,console.statusreal)
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+        else:
+            console.notmatch()
+            console.keyboard()
+            console.cmdsfind(console.cmd)
+    def consoletrdinit():
+        simthread.consolethread(1,"consolethread1",1).run()
+        print("init the consolethread")
+
+class anim:
+    def drawleg():
+        print("drawleg")
+
+if __name__=='__main__':
+    console.consoletrdinit()
